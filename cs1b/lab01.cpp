7fd4b288f103d8be5f2a361e6d36e97d338e342b
ssmith2020 <sophiagsmith410@gmail.com>
2022-08-24 10:49:53 -0700

    using pass by reference for test score problem

---
+++ cs1b/lab01.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+//function prototype
+void getTestScore(double, double, double,double,double);
+double getAverage(double,double,double,double,double);
+char grade(double);
+void displayGrade(char);
+
+//main function
+int main()
+{
+
+}
