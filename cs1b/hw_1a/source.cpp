e1becb6d46082f20a847293f03eb3e0c6faba8ff
ssmith2020 <sophiagsmith410@gmail.com>
2022-08-27 22:23:40 -0700

    finished 1a

--- cs1b/hw_1a/source.cpp
+++ cs1b/hw_1a/source.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 //function prototype
 double getsalesamt(double);
-double calccomission(double);
-double calcpay(double);
-void displaypay();
+double calccomission(double, double);
+double calcpay(double, double, double,double);
+void displaypay(double);
 
 //global variables
 
@@ -14,44 +14,62 @@ void displaypay();
 int main()
 {
     //assign variables
-    double saleamt = 0.0;
+    double salesamt = 0.0;
     double comission = 0.0;
     double pay = 0.0;
-    double total = 0.0;
+    double totalsaleamt = 0.0;
+    double total;
 
     //call function
-    saleamt = getsalesamt(double);
-    comission = calccomission(double);
-    pay = calcpay(double);
-    total = displaypay();
-    return 0;
+    
+    salesamt = getsalesamt(salesamt);
+    comission = calccomission(comission,salesamt);
+    pay = calcpay(pay, salesamt, comission, totalsaleamt);
+    displaypay(pay);
 }
 
-double getsalesamt(double saleamt)
+double getsalesamt(double salesamt)
 {
     cout << "Enter your monthly sales amount: ";
-    cin >> saleamt;
-    return saleamt;
+    cin >> salesamt;
+    return salesamt;
 }
 
 double calccomission(double commision,double salesamt)
 {
-    double rate = 0.0;
-    if (saleamt > 50000)
-        else commision = 0.02;
-    if (saleamt > 25000 || saleamt < 50000)
-        else commision = 0.015;
-    if (saleamt < 25000)
-        else comission = 0.0;
+    if (salesamt >= 50000)
+        commision = 0.02;
+    if ((salesamt < 50000) && (salesamt > 25000))
+        commision = 0.015;
+    if (salesamt < 25000)
+        commision =  0;
+        cout << "The commission is: " << commision << endl;
+    return commision; 
 }
 
-double calcpay(double pay,double salesamt)
+double calcpay(double pay,double salesamt, double commission, double totalsaleamt)
 {
-    pay = saleamt * comission; 
+    totalsaleamt = salesamt * commission;
+    pay = salesamt + totalsaleamt;
     return pay;
 }
 
-void displaypay(double total)
+void displaypay(double pay)
 {
-    cout << "the monthly salary is: " << total;
+    cout << "The monthly salary pay is: " << pay;
 }
+
+//output
+/*
+Enter your monthly sales amount: 50000
+The commission is: 0.02
+The monthly salary pay is: 51000
+
+Enter your monthly sales amount: 25000
+The commission is: 0
+The monthly salary pay is: 25000                                                                                                               
+
+Enter your monthly sales amount: 60000
+The commission is: 0.02
+The monthly salary pay is: 61200
+*/
